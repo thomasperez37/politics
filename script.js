2c380946c24faaef48fe37a51e07b5fd76a1cf58
Thomas Perez <thomasperez401@gmail.com>
2018-06-04 13:09:15 +0000

    added functions to political info based on search terms

--- script.js
+++ script.js
@@ -2,4 +2,62 @@ $(document).ready(function() {
     
 });
 
-var urlA =  "https://www.googleapis.com/civicinfo/v2/elections?key=";
+var url =  "https://www.googleapis.com/civicinfo/v2/";
+var politicsA = "elections";
+var politicsB = "representatives";
+var politicsC = "voterinfo";
+var key = "?key=AIzaSyDY2JtghVp4-UtRK2cf4bVNzk2AUPDXHTU&address=";
+
+function civicsURLWithSearchTermA(searchTerm) {
+    return url + politicsA + key + searchTerm
+}
+
+function callCivicsAPIWithSearchTermA(searchTerm) {
+    // write a function that uses the above two functions to create the url,
+    // uses ajax to send a request, and changes html of list
+    var url = civicsURLWithSearchTermA(searchTerm);
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response) {
+            
+        },
+    });
+}
+
+function civicsURLWithSearchTermB(searchTerm) {
+    return url + politicsB + key + searchTerm
+}
+
+function callCivicsAPIWithSearchTermB(searchTerm) {
+    // write a function that uses the above two functions to create the url,
+    // uses ajax to send a request, and changes html of list
+    var url = civicsURLWithSearchTermB(searchTerm);
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response) {
+            
+        },
+    });
+}
+
+function civicsURLWithSearchTermC(searchTerm) {
+    return url + politicsC + key + searchTerm
+}
+
+function callCivicsAPIWithSearchTermC(searchTerm) {
+    // write a function that uses the above two functions to create the url,
+    // uses ajax to send a request, and changes html of list
+    var url = civicsURLWithSearchTermC(searchTerm);
+    console.log(url);
+    $.ajax({
+        url: url,
+        method: "GET",
+        success: function(response) {
+            
+        },
+    });
+}
