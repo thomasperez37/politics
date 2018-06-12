4ade697cbb58c1b3accdb98c75a254a8ba56dcf6
Thomas Perez <thomasperez401@gmail.com>
2018-06-12 02:24:17 +0000

    created website to show you representatives and phone numbers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # politics
 AIzaSyDY2JtghVp4-UtRK2cf4bVNzk2AUPDXHTU
+1263 Pacific Ave. Kansas City KS
--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
-    
+    $("#search").click(function() {
+        var searchTerm = $("#srch-term").val();
+        callCivicsAPIWithSearchTermB(searchTerm);
+    });
 });
 
 var url =  "https://www.googleapis.com/civicinfo/v2/";
@@ -7,6 +10,8 @@ var politicsA = "elections";
 var politicsB = "representatives";
 var politicsC = "voterinfo";
 var key = "?key=AIzaSyDY2JtghVp4-UtRK2cf4bVNzk2AUPDXHTU&address=";
+var sub = 0;
+var dub = 0;
 
 function civicsURLWithSearchTermA(searchTerm) {
     return url + politicsA + key + searchTerm
@@ -43,13 +48,21 @@ function callCivicsAPIWithSearchTermB(searchTerm) {
         url: url,
         method: "GET",
         success: function(response) {
+            console.log("<p>Address: " + response.normalizedInput.line1 + "</p>");
             $("#list3").append("<p>Address: " + response.normalizedInput.line1 + "</p>");
             $("#list3").append("<p>City: " + response.normalizedInput.city + "</p>");
             $("#list3").append("<p>State: " + response.normalizedInput.state + "</p>");
             $("#list3").append("<p>Zip Code: " + response.normalizedInput.zip + "</p>");
             $("#list3").append("<p> </p>");
-            for(var b = 0; b < response.officials.length; b++) {
-                
+            for (var b = 0; b < response.officials.length; b++) {
+                sub = b + 1;
+                $("#list3").append("<div class=\"line\"></div>");
+                $("#list3").append("<p>Representative " + sub.toString() + ": " + response.officials[b].name + "</p>");
+                $("#list3").append("<p>Party: " + response.officials[b].party + "</p>");
+                for (var d = 0; d < response.officials[b].phones.length; d++) {
+                    dub = d + 1;
+                    $("#list3").append("<p>Phone Number " + dub.toString() + ": " + response.officials[b].phones[d] + "</p>");
+                }
             }
         },
         error: function(a,b,c) {
